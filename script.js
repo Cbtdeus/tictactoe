15402513e9bfc6edc764de9a05edd69cdcf8720e
Cbtdeus <cbtdeus@proton.me>
2024-09-29 05:19:00 +0300

    no gui version done

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 gameBoard = (function (){
     const board = ["","","","","","","","",""]
-    const change = function(piece,location){
-        if (board[location].length === 0)                     /* add legal move checking */
+    const change = function(piece,location){                   
         board[location] = piece
         console.log(board)
         }
@@ -10,18 +9,110 @@ gameBoard = (function (){
             board[i] = ""
     }
 }
-    return {change,clear}
+    const isLegal = (moveIndex) => {    
+        if (gameBoard.board[moveIndex].length == 0){
+            return true
+        }else {
+            return false
+        }
+    }
+
+     const checkWinner = (piece) => {
+        if (board.at(0) === piece && board.at(1) === piece && board.at(2) === piece){
+            return true
+        }
+        if (board.at(3) === piece && board.at(4) === piece && board.at(5) === piece){
+            return true
+        }
+        if (board.at(6) === piece && board.at(7) === piece && board.at(8) === piece){
+            return true
+        }
+        if (board.at(0) === piece && board.at(3) === piece && board.at(6) === piece){
+            return true
+        }
+        if (board.at(1) === piece && board.at(4) === piece && board.at(7) === piece){
+            return true
+        }
+        if (board.at(2) === piece && board.at(5) === piece && board.at(8) === piece){
+            return true
+        }
+        if (board.at(0) === piece && board.at(4) === piece && board.at(8) === piece){
+            return true
+        }
+        if (board.at(2) === piece && board.at(4) === piece && board.at(6) === piece){
+            return true
+        }
+        return false
+    } 
+
+    return {board,change,clear,isLegal,checkWinner}
 })()
 
-player = {
-    humanSide =
-    getBotMove                          
-    getHumanMove                /* These 2 functions should call the game.isLegalMove to check if its their turn and if the move is legal */
-}                               /* Human move should stop when the move is illegal, while bot move should loop until it finds a legal move */
-
-game = {   
-
-    isLegalMove
+const player = {
+    getSides:(playerPiece) => {
+        if (playerPiece === "X")
+            botPiece = "O"
+        else botPiece = "X"
+        return {playerPiece,botPiece}
+    },
+    getHumanMove: (humanMove) => {
+        while (gameBoard.isLegal(humanMove) === false){
+            humanMove = prompt("pick something else")
+        } 
+        return humanMove
+    },
+    getBotMove: () => {
+       botMove = Math.floor(Math.random()*8.9)
+       while (gameBoard.isLegal(botMove) === false){
+        botMove = Math.floor(Math.random()*8.9)
+       }
+        return botMove
+    }
 }
+ playGame = function(){
+    gameBoard.clear()
+    const sides = player.getSides(prompt("Pick a piece"))
+    console.log(sides)
+    for (let i = 1; i < 10;i++){
+        if (i % 2 != 0){
+            if (sides.playerPiece === "O"){
+                gameBoard.change("O",player.getHumanMove(prompt("1")))
+                if (gameBoard.checkWinner("O")){
+                    console.log("You won!")
+                    return
+                }
+            }else{
+                gameBoard.change("O",player.getBotMove())
+                if (gameBoard.checkWinner("O")){
+                    console.log("You lost!")
+                    return
+                }
+            }
+           gameBoard.checkWinner("O")
+        }
+        if (i % 2 === 0){
+            if (sides.playerPiece === "X"){
+                gameBoard.change("X",player.getHumanMove(prompt("1")))
+                if (gameBoard.checkWinner("X")){
+                    console.log("You won!")
+                    return
+                }
+            }else{
+                gameBoard.change("X",player.getBotMove())
+            }
+            if (gameBoard.checkWinner("X")){
+                if (gameBoard.checkWinner("X")){
+                    console.log("You Lost!")
+                    return
+                }
+                
+            }
+        }
+        if (i === 9){
+            console.log("Tie")
+        } 
+        }
+    
+}   
 
-playGame = {}          /* add checking whose turn it is before altering the board */
+playGame()
