a1918e3db8a4f6cfcdfc4b53644d13d3a282cfbe
Cbtdeus <cbtdeus@proton.me>
2024-10-05 03:28:19 +0300

    added ui, game status

--- script.js
+++ script.js
@@ -7,21 +7,34 @@ gameBoard = {
         gameBoard.board.fill("")
     }
     }
+player = {
+    O:"O",
+    X:"X"
+}
 game = {
     pause:false,
     round:1,
+    
     playRound:(index) => {
-    if (index > 8 || index < 0 || gameBoard.board.at(index) != "" || game.pause === true) {
+    if (game.round > 9){
+        pause = true
+        console.log("Draw!")
+        displayHandler.gameStatus.textContent = "Draw!"
+    }
+    if (index > 8 || index < 0 || gameBoard.board.at(index) != "" || game.pause == true) {
         return
     }
     if (game.round % 2){
         gameBoard.change("O",index)             
-        game.checkWinner()
+        game.checkWinner(player.O)
+        displayHandler.gameStatus.textContent = player.X + "'s Turn"
     }else{
         gameBoard.change("X",index)                
-        game.checkWinner()
+        game.checkWinner(player.X)
+        displayHandler.gameStatus.textContent = player.O + "'s Turn"
     }
     console.log(gameBoard.board)
+    console.log(game.round)
     game.round++
     },
     reset:() => {
@@ -39,7 +52,7 @@ game = {
         [0,4,8],
         [2,4,6]
     ],
-    checkWinner:() => {
+    checkWinner:(winner) => {
         for (let i = 0; i < game.winConditions.length;i++){
             const condition = game.winConditions[i]
             const cellA = gameBoard.board[condition[0]]
@@ -49,9 +62,37 @@ game = {
                 continue
             }
             if (cellA == cellB  && cellB == cellC){
-                console.log(gameBoard.board[condition[2]] + " won!")
+                displayHandler.gameStatus.textContent = winner + " Wins!"
+                console.log(winner + " Wins!") 
                 game.pause = true
             }
         }
     }
-    }   
+    }
+displayHandler = {
+    refreshCell:(id) => {
+        document.getElementById(id).textContent = gameBoard.board[id]
+    },
+    refreshBoard:() => {
+        for (let i = 0; i < 9; i++){ 
+        displayHandler.refreshCell(i)
+        }
+    },
+    gameStatus:document.querySelector(".status")
+} 
+boardContainer = document.querySelector(".boardContainer")
+for (let i = 0; i < 9;i++){
+    const cell = document.createElement("div")
+    cell.setAttribute("class","cell")
+    cell.setAttribute("id",i)
+    boardContainer.appendChild(cell)
+    cell.addEventListener("click", () => {
+        game.playRound(i)
+        displayHandler.refreshCell(i)
+    })
+}
+resetButton = document.querySelector("#reset")
+resetButton.addEventListener("click", () => {
+    gameBoard.clear()
+    displayHandler.refreshBoard()
+})
